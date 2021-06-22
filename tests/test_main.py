a009556bec89849e71b0fd32a6270989b1f90493
Magnus Axelsson <magnus@simula.no>
2021-06-22 12:16:34 +0200

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,14 +2,12 @@ import pytest
 from pathlib import Path
 import sys
 
-root_folder = str(Path.cwd().resolve().parent)
+root_folder = str(Path(__file__).resolve().parent.parent)
 sys.path.insert(0, root_folder)
-
 from main import get_data  # noqa: E402
 
 
 @pytest.fixture(scope="module")
 def test_check_datatypes():
-
     dataset = get_data()
     assert dataset.shape[1] == 100
